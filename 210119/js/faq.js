9afb242883e7bf3e097363ebaaec2f852a381a40
zeeuoo <45193893+zeeuoo@users.noreply.github.com>
2021-01-19 17:25:38 +0900

    210119 수정
    - 셰프 프로필 추가
    - 메뉴 note 추가
    - 데코 프리미엄 이미지 변경

---
+++ 210119/js/faq.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+/*footer mobile*/
+if($(window).width() <991) { 
+    $('#footer .contact p a.kakao span').html('카카오톡 <em><a href="http://pf.kakao.com/_xcxbiFK/chat">@그로토</a></em> 채널 추가하고 자세한 상담 받아보세요.');
+}
+    
+})
